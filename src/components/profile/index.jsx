f19303ada6c35f9fe6bb9abe031eeeb43c760091
kadraknb <kadrak02@hotmail.com>
2024-02-20 00:33:15 -0300

    removendo div, alterando class

--- src/components/profile/index.jsx
+++ src/components/profile/index.jsx
@@ -10,22 +10,20 @@ function Profile() {
   const { ajusteTelaWidth } = useContext(Context);
 
   return (
-    <div id="box_profile_main" className="efeito_vidro" style={ajusteTelaWidth}>
-      <div>
-        <div id="box_img_perf">
-          <img
-            src={photoProfile}
-            alt="photoProfile"
-            className="img_Profile tamanho_img_perf"
-          />
-          <div className="glass_Img tamanho_img_perf"></div>
-        </div>
-
-        <div>
-          <h5 className="nome_perf flax_center">Vagner Cardoso dos Santos</h5>
-        </div>
-        <Contatos />
+    <div id="box_profile_main" className="sombra" style={ajusteTelaWidth}>
+      <div id="box_img_perf">
+        <img
+          draggable="false"
+          src={photoProfile}
+          alt="photoProfile"
+          className="img_Profile"
+        />
       </div>
+      <div>
+        <h5 className="nome_perf flax_center">Vagner Cardoso dos Santos</h5>
+      </div>
+
+      <Contatos />
       <IconsTec />
     </div>
   );
